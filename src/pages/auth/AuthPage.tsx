4e2cb0dec47093a01a2cb26c4939343b43bc4e86
NPhucs <phucnguyen11204@gmail.com>
2025-08-09 21:37:33 +0700

    sửa lại container

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -3,7 +3,7 @@ import Register from "./register/Register";
 
 export default function AuthPage() {
   return (
-    <div className="container grid grid-cols-2 gap-[5%] pt-[5%]">
+    <div className="max-w-default default:mx-auto mx-8 grid grid-cols-2 gap-[5%] pt-[5%]">
       <Login />
       <Register />
     </div>
